274969a06b8d7a41687076dfb2ae8d82e3ff2e80
Dung D. Phan <brianp.dung@utexas.edu>
2023-10-11 16:08:40 -0500

    Update DetectorConstruction.cpp

--- src/DetectorConstruction.cpp
+++ src/DetectorConstruction.cpp
@@ -28,25 +28,25 @@ G4VPhysicalVolume *DetectorConstruction::Construct() {
     VAWorld->SetColour(1., 1., 1., 0.2);
     WorldLogicalVolume->SetVisAttributes(VAWorld);
 
-//    // Shielding volume
-//    ShieldingMaterial.reset(NISTManager->FindOrBuildMaterial("G4_Pb"));
-//    ShieldingSphere = std::make_unique<G4Sphere>("ShieldingSphere", DistanceFromInteractionPointToShielding, DistanceFromInteractionPointToShielding + ShieldingThickness, 0., 360. * deg, 0., 180. * deg);
-//    ShieldingLogicalVolume = std::make_unique<G4LogicalVolume>(ShieldingSphere.get(), ShieldingMaterial.get(), "ShieldingLogicalVolume");
-//    ShieldingPhysicalVolume = std::make_unique<G4PVPlacement>(nullptr, G4ThreeVector(), ShieldingLogicalVolume.get(), "ShieldingPhysicalVolume", WorldLogicalVolume.get(), false, 0, CheckOverlaps);
-//    auto VAShielding = new G4VisAttributes();
-//    VAShielding->SetForceSolid();
-//    VAShielding->SetColour(0., 1., 0., 0.5);
-//    ShieldingLogicalVolume->SetVisAttributes(VAShielding);
-//
-//    // Detector volume
-//    DetectorMaterial.reset(NISTManager->FindOrBuildMaterial("G4_Pb"));
-//    DetectorSphere = std::make_unique<G4Sphere>("DetectorSphere", DistanceFromInteractionPointToDetector, DistanceFromInteractionPointToDetector + DetectorThickness, 0., 360. * deg, 0., 180. * deg);
-//    DetectorLogicalVolume = std::make_unique<G4LogicalVolume>(DetectorSphere.get(), DetectorMaterial.get(), "DetectorLogicalVolume");
-//    DetectorPhysicalVolume = std::make_unique<G4PVPlacement>(nullptr, G4ThreeVector(), DetectorLogicalVolume.get(), "DetectorPhysicalVolume", WorldLogicalVolume.get(), false, 0, CheckOverlaps);
-//    auto VADetector = new G4VisAttributes();
-//    VADetector->SetForceSolid();
-//    VADetector->SetColour(1., 0., 0., 0.9);
-//    DetectorLogicalVolume->SetVisAttributes(VADetector);
+    // Shielding volume
+    ShieldingMaterial.reset(NISTManager->FindOrBuildMaterial("G4_Pb"));
+    ShieldingSphere = std::make_unique<G4Sphere>("ShieldingSphere", DistanceFromInteractionPointToShielding, DistanceFromInteractionPointToShielding + ShieldingThickness, 0., 360. * deg, 0., 180. * deg);
+    ShieldingLogicalVolume = std::make_unique<G4LogicalVolume>(ShieldingSphere.get(), ShieldingMaterial.get(), "ShieldingLogicalVolume");
+    ShieldingPhysicalVolume = std::make_unique<G4PVPlacement>(nullptr, G4ThreeVector(), ShieldingLogicalVolume.get(), "ShieldingPhysicalVolume", WorldLogicalVolume.get(), false, 0, CheckOverlaps);
+    auto VAShielding = new G4VisAttributes();
+    VAShielding->SetForceSolid();
+    VAShielding->SetColour(0., 1., 0., 0.5);
+    ShieldingLogicalVolume->SetVisAttributes(VAShielding);
+
+    // Detector volume
+    DetectorMaterial.reset(NISTManager->FindOrBuildMaterial("G4_Pb"));
+    DetectorSphere = std::make_unique<G4Sphere>("DetectorSphere", DistanceFromInteractionPointToDetector, DistanceFromInteractionPointToDetector + DetectorThickness, 0., 360. * deg, 0., 180. * deg);
+    DetectorLogicalVolume = std::make_unique<G4LogicalVolume>(DetectorSphere.get(), DetectorMaterial.get(), "DetectorLogicalVolume");
+    DetectorPhysicalVolume = std::make_unique<G4PVPlacement>(nullptr, G4ThreeVector(), DetectorLogicalVolume.get(), "DetectorPhysicalVolume", WorldLogicalVolume.get(), false, 0, CheckOverlaps);
+    auto VADetector = new G4VisAttributes();
+    VADetector->SetForceSolid();
+    VADetector->SetColour(1., 0., 0., 0.9);
+    DetectorLogicalVolume->SetVisAttributes(VADetector);
 
     return WorldPhysicalVolume.get();
 }
