59da1731e2476a2be592051cb5e9f483bd594100
Dung D. Phan <brianp.dung@utexas.edu>
2023-10-18 13:34:36 -0500

    Update DetectorConstruction.h

--- inc/DetectorConstruction.h
+++ inc/DetectorConstruction.h
@@ -9,12 +9,20 @@
 #include <G4Sphere.hh>
 #include <G4Box.hh>
 #include <G4LogicalVolume.hh>
+#include <G4VPhysicalVolume.hh>
 #include <G4PVPlacement.hh>
 #include <G4SystemOfUnits.hh>
 #include <G4SDManager.hh>
 #include <G4NistManager.hh>
 #include <G4Isotope.hh>
 #include <G4VisAttributes.hh>
+#include <G4Colour.hh>
+#include <G4PVReplica.hh>
+#include <G4MultiFunctionalDetector.hh>
+#include <G4VPrimitiveScorer.hh>
+#include <G4THitsMap.hh>
+#include <G4VIStore.hh>
+
 
 #include <TMath.h>
 
@@ -26,14 +34,15 @@ public:
     G4VPhysicalVolume* Construct() override;
     void ConstructSDandField() override;
 
-    void CreateImportanceStore();
+    G4VIStore* CreateImportanceStore();
+    G4VPhysicalVolume* GetWorldVolume();
 
     void DefineMaterials();
 
 private:
     bool CheckOverlaps;
 
-    std::unique_ptr<G4NistManager> NISTManager;
+    G4NistManager* NISTManager;
 
     G4double WorldSize;
     G4double TotalHeavyWaterThickness;
@@ -42,29 +51,29 @@ private:
     G4double HeavyWaterCellThickness;
     G4double ShieldingCellThickness;
     G4double DetectorCellThickness;
+    G4double VoidThickness;
     G4int    NumberOfHeavyWaterLayers;
     G4int    NumberOfShieldingLayers;
     G4int    NumberOfDetectorLayers;
 
+    G4Material* WorldMaterial;
+    G4Material* HeavyWaterResidualMaterial;
+    G4Material* ShieldingMaterial;
+    G4Material* DetectorMaterial;
 
-    std::unique_ptr<G4Material> WorldMaterial;
-    std::unique_ptr<G4Material> HeavyWaterResidualMaterial;
-    std::unique_ptr<G4Material> ShieldingMaterial;
-    std::unique_ptr<G4Material> DetectorMaterial;
+    G4Sphere* WorldSphere;
+    G4LogicalVolume* WorldLogicalVolume;
+    G4VPhysicalVolume* WorldPhysicalVolume;
 
-    std::unique_ptr<G4Sphere> WorldSphere;
-    std::unique_ptr<G4LogicalVolume> WorldLogicalVolume;
-    std::unique_ptr<G4PVPlacement> WorldPhysicalVolume;
+    std::vector<G4Sphere*> HeavyWaterResidualSphere;
+    std::vector<G4LogicalVolume*> HeavyWaterResidualLogicalVolume;
+    std::vector<G4VPhysicalVolume*> HeavyWaterResidualPhysicalVolume;
 
-    std::vector<std::unique_ptr<G4Sphere>> HeavyWaterResidualSphere;
-    std::vector<std::unique_ptr<G4LogicalVolume>> HeavyWaterResidualLogicalVolume;
-    std::vector<std::unique_ptr<G4PVPlacement>> HeavyWaterResidualPhysicalVolume;
+    std::vector<G4Sphere*> ShieldingSphere;
+    std::vector<G4LogicalVolume*> ShieldingLogicalVolume;
+    std::vector<G4VPhysicalVolume*> ShieldingPhysicalVolume;
 
-    std::vector<std::unique_ptr<G4Sphere>> ShieldingSphere;
-    std::vector<std::unique_ptr<G4LogicalVolume>> ShieldingLogicalVolume;
-    std::vector<std::unique_ptr<G4PVPlacement>> ShieldingPhysicalVolume;
-
-    std::vector<std::unique_ptr<G4Sphere>> DetectorSphere;
-    std::vector<std::unique_ptr<G4LogicalVolume>> DetectorLogicalVolume;
-    std::vector<std::unique_ptr<G4PVPlacement>> DetectorPhysicalVolume;
+    std::vector<G4Sphere*> DetectorSphere;
+    std::vector<G4LogicalVolume*> DetectorLogicalVolume;
+    std::vector<G4VPhysicalVolume*> DetectorPhysicalVolume;
 };
