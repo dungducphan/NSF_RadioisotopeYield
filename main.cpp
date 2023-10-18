546c46e4d26788f174aa76ab4e884e8d98923574
Dung D. Phan <brianp.dung@utexas.edu>
2023-10-18 16:25:57 -0500

    Applying importance biasing depending on the energy input

--- main.cpp
+++ main.cpp
@@ -32,16 +32,19 @@ int main(int argc, char **argv) {
 
     auto detectorConstruction = new DetectorConstruction();
     runManager->SetUserInitialization(detectorConstruction);
-    G4GeometrySampler mgs(detectorConstruction->GetWorldVolume(),"deuteron");
     auto physicsList = new QGSP_BIC_AllHP();
-//     physicsList->RegisterPhysics(new BiasingPhysics());
-    physicsList->RegisterPhysics(new G4ImportanceBiasing(&mgs));
+    if (energyInMeV >= 6 && energyInMeV <= 16) {
+        G4GeometrySampler mgs(detectorConstruction->GetWorldVolume(),"deuteron");
+        physicsList->RegisterPhysics(new G4ImportanceBiasing(&mgs));
+    }
     runManager->SetUserInitialization(physicsList);
     runManager->SetUserInitialization(new ActionInitialization(energyInMeV));
 
     runManager->SetNumberOfThreads(24);
     runManager->Initialize();
-    detectorConstruction->CreateImportanceStore();
+    if (energyInMeV >= 6 && energyInMeV <= 16) {
+        detectorConstruction->CreateImportanceStore();
+    }
 
     G4VisManager *visManager = new G4VisExecutive;
     visManager->Initialize();
