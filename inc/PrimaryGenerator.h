e5cd0d1b487e7498dbb09ac1f9a4465c4505d67d
Dung D. Phan <brianp.dung@utexas.edu>
2023-10-18 13:34:41 -0500

    Update PrimaryGenerator.h

--- inc/PrimaryGenerator.h
+++ inc/PrimaryGenerator.h
@@ -16,11 +16,11 @@
 
 class PrimaryGenerator : public G4VUserPrimaryGeneratorAction{
 public:
-    PrimaryGenerator();
+    PrimaryGenerator(const double &energyInMeV);
     ~PrimaryGenerator() override;
 
     void GeneratePrimaries(G4Event* anEvent) override;
 
 private:
-    std::unique_ptr<G4GeneralParticleSource> GeneralParticleSource;
+    G4GeneralParticleSource* GeneralParticleSource;
 };
