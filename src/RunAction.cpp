ca8c3fa00fa5a2c9c8eec77c97f22ce941201c4b
Dung D. Phan <brianp.dung@utexas.edu>
2023-10-18 13:34:57 -0500

    Update RunAction.cpp

--- src/RunAction.cpp
+++ src/RunAction.cpp
@@ -1,15 +1,25 @@
 #include <RunAction.h>
 
-RunAction::RunAction() : G4UserRunAction() {
-    AnalysisManager.reset(G4AnalysisManager::Instance());
+#include <TString.h>
+
+RunAction::RunAction(const double &energyInMeV) : G4UserRunAction(), PrimaryEnergyInMeV(energyInMeV) {
+    G4AnalysisManager* AnalysisManager = G4AnalysisManager::Instance();
+    if (G4Threading::IsMultithreadedApplication()) AnalysisManager->SetNtupleMerging(true);
+
+    AnalysisManager->CreateNtuple("DeuteronNTuple", "DeuteronNTuple");
+    AnalysisManager->CreateNtupleDColumn("Energy");
+    AnalysisManager->FinishNtuple(0);
 }
 
 RunAction::~RunAction() = default;
 
 void RunAction::BeginOfRunAction(const G4Run* run) {
-    G4cout << "### Run " << run->GetRunID() << " start." << G4endl;
+    G4AnalysisManager* AnalysisManager = G4AnalysisManager::Instance();
+    AnalysisManager->OpenFile(Form("DeuteronData_With_%02i_MeV_Deuterons.root", (int) PrimaryEnergyInMeV));
 }
 
 void RunAction::EndOfRunAction(const G4Run* run) {
-    G4cout << "### Run " << run->GetRunID() << " end." << G4endl;
+    G4AnalysisManager* AnalysisManager = G4AnalysisManager::Instance();
+    AnalysisManager->Write();
+    AnalysisManager->CloseFile();
 }
